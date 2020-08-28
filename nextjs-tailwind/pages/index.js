9872c0b60bd82dd48d0431b717ba485976898364
DSK <sureshdsk91@gmail.com>
2020-08-28 21:00:23 +0530

    tailwind custom utility components

--- nextjs-tailwind/pages/index.js
+++ nextjs-tailwind/pages/index.js
@@ -14,7 +14,8 @@ export default function IndexPage() {
             Workcation helps you find work-friendly rentals in beautiful locations so you can enjoy some nice weather even when you're not on vacation.
           </p>
           <div className="mt-4 sm:mt-6">
-            <a href="#" className="inline-block px-5 py-3 rounded-lg shadow-lg bg-indigo-500 text-sm text-white uppercase tracking-wider font-semibold sm:text-base">Book your escape</a>
+            <a href="#" className="btn btn-indigo shadow-lg sm:text-base">Book your escape</a>
+            <a href="#" className="ml-2 btn btn-gray sm:text-base">Learn more</a>
           </div>
         </div>
       </div>
--- nextjs-tailwind/tailwind.config.js
+++ nextjs-tailwind/tailwind.config.js
@@ -7,6 +7,8 @@ module.exports = {
       },
     },
   },
-  variants: {},
+  variants: {
+    backgroundColor: ['responsive', 'hover', 'focus', 'active'],
+  },
   plugins: [],
 }
