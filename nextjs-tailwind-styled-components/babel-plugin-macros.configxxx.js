91d4161bcce8a289339be5285c1834ddf070d52d
DSK <sureshdsk91@gmail.com>
2020-08-30 21:18:58 +0530

    tailwind styled components

---
+++ nextjs-tailwind-styled-components/babel-plugin-macros.configxxx.js
@@ -0,0 +1,11 @@
+module.exports = {
+    tailwind: {
+      plugins: ["macros"],
+      config: "tailwind.config.js",
+      // styled: 'styled-components/macro',
+      format: "auto",
+    },
+    styledComponents: {
+      pure: true,
+    },
+  }
---
+++ nextjs-tailwind-styled-components/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}','./xstyles/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'accent-1': '#333',
+      },
+    },
+  },
+  variants: {
+    backgroundColor: ['responsive', 'hover', 'focus', 'active'],
+  },
+  plugins: [],
+}
