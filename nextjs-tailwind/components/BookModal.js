5abcdb663c5966d5f0537e3bd01e074f3975613a
DSK <sureshdsk91@gmail.com>
2020-09-05 12:41:18 +0530

    transition effect

--- nextjs-tailwind/components/BookModal.js
+++ nextjs-tailwind/components/BookModal.js
@@ -1,12 +1,20 @@
-import { useState } from 'react';
+import { useState } from 'react'
+import { Transition } from '@tailwindui/react'
 
 
 export default function BookModal({showModal, setShowModal}) {
 
     return (
       <>
-      {showModal ? (
-            <>
+      <Transition
+            show={showModal}
+            enter="transition-opacity duration-100"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="transition-opacity duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+        >
             <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none" onClick={() => setShowModal(false)}>
                 <div className="relative w-auto my-6 mx-auto max-w-3xl">
                 <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
@@ -48,8 +56,7 @@ export default function BookModal({showModal, setShowModal}) {
                 </div>
             </div>
             <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-            </>
-        ) : ''}
+        </Transition>
         </>
     )
 }
